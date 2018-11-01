213c14ce785cb18a0196f8b2e1ffc2c7fedefdeb
omar <oalkersh@ymail.com>
2018-10-31 20:09:25 -0400

    Added tests for polyToPoly functions

--- src/Assign_3_ExtraCredit.hs
+++ src/Assign_3_ExtraCredit.hs
@@ -11,6 +11,26 @@ data PolyAlt a = Monomial a Integer
                | SumAlt (PolyAlt a) (PolyAlt a)
   deriving Show
 
+{-------------------------------------------------------------------
+  -Takes a polynomial function, PolyAlt, and a number, Num a => n
+  -and returns result from subbing n into the function
+  -example:
+  -(Monomial 5 2) => 5*x^2
+  -n=5 => 5(5)^2 = 125
+  --}
+--TODO: replace tests
+{- -----------------------------------------------------------------
+  - Test Cases
+  - -----------------------------------------------------------------
+  - polyDeriv
+  - -----------------------------------------------------------------
+  - - Function: polyDeriv
+  - - Test Case Number: 1-A
+  - - Input: X
+  - - Expected Output: Coef 1
+  - - Acutal Output: Coef 1
+  - -----------------------------------------------------------------
+  -}
 polyAltValue:: Num a => PolyAlt a -> a -> a
 polyAltValue (Monomial x y) n = x*n ^ abs y
 polyAltValue (SumAlt x y) n = polyAltValue x n + polyAltValue y n
@@ -47,6 +67,29 @@ polyToPolyAlt (Sum x y) = SumAlt (polyToPolyAlt x) (polyToPolyAlt y)
 polyToPolyAlt (Prod x y) = polyAltProd (polyToPolyAlt x) (polyToPolyAlt y)
 
 polyAltToPoly :: (Num a, Eq a) => PolyAlt a -> Poly a
-polyAltToPoly (Monomial x y) | y == 1 = Prod X (Coef x)
-                             | otherwise = Prod X (polyAltToPoly (Monomial x (y-1)))
+polyAltToPoly (Monomial x y) | y == 0 = Coef x
+                             | y > 0 = Prod X (polyAltToPoly (Monomial x (y-1)))
 polyAltToPoly (SumAlt x y) = Sum (polyAltToPoly x) (polyAltToPoly y)
+
+(-->) :: Bool -> Bool -> Bool
+a --> b = not a || b
+
+--tests polyAltToPoly function
+polyAltToPolyCheck1 :: (Num a, Eq a) =>(a, Integer, a)-> Bool
+polyAltToPolyCheck2 :: (Num a, Eq a) => (a, Integer) -> (a, Integer) -> a -> Bool
+polyAltToPolyCheck1 (c,e,n) = let
+  p = Monomial c e
+  in (e > 0) --> (polyAltValue p n == polyValue (polyAltToPoly p) n)
+polyAltToPolyCheck2 (c1,e1) (c2,e2) n =let
+  p = SumAlt (Monomial c1 e1) (Monomial c2 e2)
+  in ((e1>0) && (e2>0)) --> (polyAltValue p n == polyValue (polyAltToPoly p) n)
+
+--tests polyToPolyAlt function
+polyToPolyAltCheck1 :: (Num a, Eq a) => a -> Integer -> a -> Bool
+polyToPolyAltCheck2 :: (Num a, Eq a) => (a, Integer) -> (a, Integer) -> a -> Bool
+polyToPolyAltCheck1 c e n = let
+  p = Monomial c e
+  in (e>0) --> (polyAltValue(polyToPolyAlt(polyAltToPoly p)) n == polyAltValue p n)
+polyToPolyAltCheck2 (c1,e1) (c2,e2) n = let
+  p = SumAlt (Monomial c1 e1) (Monomial c2 e2)
+  in (e1>0 && e2>0) --> ( polyAltValue(polyToPolyAlt(polyAltToPoly p)) n == polyAltValue p n)
