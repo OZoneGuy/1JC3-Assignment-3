2d5eb464a732a4430f1b61ebe4a43c7c3132ac9d
omar <oalkersh@ymail.com>
2018-11-04 15:38:02 -0500

    Added comment to all functions except polyToPolyAlt and polyAltToPoly

--- src/Assign_3_ExtraCredit.hs
+++ src/Assign_3_ExtraCredit.hs
@@ -12,43 +12,75 @@ data PolyAlt a = Monomial a Integer
   deriving Show
 
 {-------------------------------------------------------------------
-  -Takes a polynomial function, PolyAlt, and a number, Num a => n
-  -and returns result from subbing n into the function
-  -example:
-  -(Monomial 5 2) => 5*x^2
-  -n=5 => 5(5)^2 = 125
-  --}
---TODO: replace tests
-{- -----------------------------------------------------------------
-  - Test Cases
-  - -----------------------------------------------------------------
-  - polyDeriv
-  - -----------------------------------------------------------------
-  - - Function: polyDeriv
-  - - Test Case Number: 1-A
-  - - Input: X
-  - - Expected Output: Coef 1
-  - - Acutal Output: Coef 1
-  - -----------------------------------------------------------------
-  -}
+ - polyAltValue
+ -------------------------------------------------------------------
+ - Takes a polynomial function, PolyAlt, and a number, Num a => n
+ - and returns result from subbing n into the function
+ - example:
+ - (Monomial 5 2) => 5*x^2
+ - n=5 => 5(5)^2 = 125
+ -}
+
 polyAltValue:: Num a => PolyAlt a -> a -> a
 polyAltValue (Monomial x y) n = x*n ^ abs y
 polyAltValue (SumAlt x y) n = polyAltValue x n + polyAltValue y n
 
+{-------------------------------------------------------------------
+ - polyAltDegree
+ -------------------------------------------------------------------
+ - Finds the highest exponent in a PolyAlt a polyValue
+ - For any Monomial a b the degree is b
+ - for any SumAlt x y the degree is equal to highest between x and y
+ - where x amd y are PolyAlt
+ -}
+
 polyAltDegree:: Num a => PolyAlt a ->  Integer
 polyAltDegree (Monomial _ y) = y
 polyAltDegree (SumAlt x y) = max (polyAltDegree x) (polyAltDegree y)
+
+{-------------------------------------------------------------------
+ - polyAltDeriv
+ -------------------------------------------------------------------
+ - Uses dereivation rules to find the derivative of PolyAlt data
+ - for Monomial a b it uses the power rule
+ - for SumAlt x y the funstion employs recursion
+ -}
 
 polyAltDeriv :: Num a => PolyAlt a -> PolyAlt a
 polyAltDeriv (Monomial x y) | y == 0 = Monomial 0 0
                             | otherwise = Monomial (x * fromInteger y) (y-1)
 polyAltDeriv (SumAlt x y) = SumAlt (polyAltDeriv x) (polyAltDeriv y)
 
+{-------------------------------------------------------------------
+ - polyAltProd
+ -------------------------------------------------------------------
+ - Miltiplies two PolyAlt values using aithmatic laws:
+ - x*y => x*y
+ - x(a+b) => a*x + b*x
+ - (a+b)(x+y) => a*x + a*y + b*x + b*y
+ - This function can emplyes recursion where Monomial a b is the simplest case
+ -}
+
 polyAltProd :: Num a => PolyAlt a -> PolyAlt a -> PolyAlt a
 polyAltProd (Monomial x y) (Monomial a b) = Monomial (x*a) (y+b)
 polyAltProd (Monomial x y) (SumAlt a b) = SumAlt (polyAltProd (Monomial x y) a) (polyAltProd (Monomial x y) b)
 polyAltProd (SumAlt a b) (Monomial x y) = SumAlt (polyAltProd (Monomial x y) a) (polyAltProd (Monomial x y) b)
 polyAltProd (SumAlt a b) (SumAlt x y) = SumAlt (SumAlt (polyAltProd a x) (polyAltProd a y)) (SumAlt (polyAltProd b x) (polyAltProd b y))
+
+{-------------------------------------------------------------------
+ - polyAltNewton
+ -------------------------------------------------------------------
+ - estimates the root of the equation using Newton's method -> http://tutorial.math.lamar.edu/Classes/CalcI/NewtonsMethod.aspx
+ - In short, newtons method starts with a point on the graph, x1, and uses the tangent of the graph on that point to estimate the second point, x2, which is where the tangent corsses the x-axis, the same procedure is repeated using the second point until the difference between xn and xn-1 is <= tolerance, which predefined
+ - The original point and the function are already given, s and p respectively
+ - the second point is found using y=mx+b general equation
+ - m, gradient is found using polyAltDeriv and polyAltValue
+ - b is found by rearranging the y=mx+b
+ - the y is found using polyAltValue
+ - the difference between s and x2 is compared to the tolerance, t
+ - if it is less than the tolerance the x2 is returned
+ - other the whole procedure is repeated using x2 as the starting point
+ -}
 
 polyAltNewton :: (Fractional a, Ord a) => PolyAlt a -> a -> a -> a
 polyAltNewton p s t = let
@@ -60,11 +92,23 @@ polyAltNewton p s t = let
     then x2
     else polyAltNewton p x2 t
 
+{-------------------------------------------------------------------
+ - polyToPolyAlt
+ -------------------------------------------------------------------
+ -
+ -}
+
 polyToPolyAlt :: (Num a, Eq a) => Poly a -> PolyAlt a
 polyToPolyAlt X = Monomial 1 1
 polyToPolyAlt (Coef x) = Monomial x 0
 polyToPolyAlt (Sum x y) = SumAlt (polyToPolyAlt x) (polyToPolyAlt y)
 polyToPolyAlt (Prod x y) = polyAltProd (polyToPolyAlt x) (polyToPolyAlt y)
+
+{-------------------------------------------------------------------
+ - polyAltToPoly
+ -------------------------------------------------------------------
+ -
+ -}
 
 polyAltToPoly :: (Num a, Eq a) => PolyAlt a -> Poly a
 polyAltToPoly (Monomial x y) | y == 0 = Coef x
